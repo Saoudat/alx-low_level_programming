caa1bbbec5ee9199aae25cbc37ebff5874a02bcd
Saoudat <halima.bizo@ansi.ne>
2023-04-23 10:22:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 y = n % 10;
 if (n > 5)
 {
-printf("the last digit of %d is %d and is greater than 5\n", n, y);
+printf("last digit of %d is %d and is greater than 5\n", n, y);
 }
 else if (n == 0)
 {
-printf("the last digit of %d is %d and is 0\n", n, y);
+printf("last digit of %d is %d and is 0\n", n, y);
 }
 else
 {
-printf("the last digit of %d is %d and is less than 6 and not 0\n", n, y);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, y);
 }
 return (0);
 }
