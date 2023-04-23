20b96de56572d745abd4b20dbdfc2bb80b7356d3
Saoudat <halima.bizo@ansi.ne>
2023-04-23 09:47:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,18 +11,18 @@ int x;
 int y;
 srand(time(0));
 x = rand() - RAND_MAX / 2;
-y = x % 100;
+y = x % 10;
 if (x > 5)
 {
-printf("the last gigit of %d is %d and is greater than 5\n", x, y);
+printf("the last gigit of %d is greater than 5\n", x, y);
 }
 else if (x == 0)
 {
-printf("the last digit of %d is %d ad is 0\n", x, y);
+printf("the last digit of %d is 0\n", x, y);
 }
 else
 {
-printf("the last digit of %d is %d and is less than 6 and not 0\n", x, y);
+printf("the last digit of %d is less than 6 and not 0\n", x, y);
 }
 return (0);
 }
