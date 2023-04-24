16471b033d1a7bbe28e96bc69e06728d95208e87
Saoudat <halima.bizo@ansi.ne>
2023-04-24 04:22:07 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - prints alphabetic in lawer case
+ * main - print alphabet in lawercase
  * Return: always 0
  */
 int main(void)
 {
+
 char lc;
 
 for (lc = 'a'; lc <= 'z'; lc++);
 {
 putchar(lc);
 }
-
 putchar('\n');
 
 retrun (0);
