e72b302763e8fb4a289bff3e2a764ed0d4b5f855
Saoudat <halima.bizo@ansi.ne>
2023-04-24 08:07:32 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <sdtio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: always 0
+ */
+int main(void)
+{
+char le;
+for (le = 'z'; le >= 'a'; le--)
+putchar(le);
+putchar('\n');
+return (0);
+}
